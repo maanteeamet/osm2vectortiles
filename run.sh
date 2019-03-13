ae26b1fed6f3d7686364125f26612e3b0427a33b
Martin Tõnts <martintonts@gmail.com>
2019-03-13 11:00:07 +0200

    commented out azure stuff and replaced osm url and BBOX coordinates to match estonia

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@ set -e
 
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-export AZURE_STORAGE_ACCOUNT=hsltiles
-export AZURE_STORAGE_ACCESS_KEY=
+#export AZURE_STORAGE_ACCOUNT=hsltiles
+#export AZURE_STORAGE_ACCESS_KEY=
 
 export CONTAINER_NAME=tiles
 export BLOB_NAME=tiles.mbtiles
@@ -12,8 +12,8 @@ export FILENAME=export/tiles.mbtiles
 export MIN_SIZE=660000000
 
 rm -f export/tiles.mbtiles
-rm -f import/finland-latest.osm.pbf
-curl -sSfL "http://dev.hsl.fi/osm.finland/finland.osm.pbf" -o import/finland-latest.osm.pbf
+rm -f import/estonia-latest.osm.pbf
+curl -sSfL "http://download.geofabrik.de/europe/estonia-latest.osm.pbf" -o import/estonia-latest.osm.pbf
 
 docker-compose stop
 docker-compose rm -f
@@ -28,7 +28,7 @@ docker-compose up import-osm
 
 docker-compose run import-sql
 
-docker-compose run -e BBOX="18.9832098,59.3541578,31.6867044,70.1922939" -e MIN_ZOOM="0" -e MAX_ZOOM="14" export
+docker-compose run -e BBOX="21.3144,57.4818,28.335,60.0013" -e MIN_ZOOM="0" -e MAX_ZOOM="14" export
 
 docker volume rm $(docker volume ls)
 
@@ -42,6 +42,6 @@ if [ $(wc -c <"$FILENAME") -lt $MIN_SIZE ]; then
     exit 1
 fi
 
-echo "Uploading..."
-az storage blob upload -f $FILENAME -c $CONTAINER_NAME -n $BLOB_NAME
-echo "Done"
+#echo "Uploading..."
+#az storage blob upload -f $FILENAME -c $CONTAINER_NAME -n $BLOB_NAME
+#echo "Done"
